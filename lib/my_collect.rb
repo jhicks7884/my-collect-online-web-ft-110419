78d07683230228a2da5c10aa1baedf56b07ab0b7
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-06 22:03:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,9 @@
-my_collect
-my_collect(array) do |x|
-  counter += 1
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
